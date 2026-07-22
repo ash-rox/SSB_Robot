9ebf954bfc475423b11c2642eeeb4d526ad9a405
ashrox <ashtraddis@gmail.com>
2026-07-22 11:36:34 -0700

    my_third_robot.py file

--- my_second_robot.py
+++
@@ -1,9 +0,0 @@
-import time
-from sparkybotmini import SparkyBotMini
-robot = SparkyBotMini(port="/dev/ttyUSB0")
-if robot.connect():
-  print("Successfully connected to SparkyBotMini!")
-  print("Playing startup sound...")
-  robot.beep(300) # Beep for 300 milliseconds
-  time.sleep(1) # Wait for 1 second
-robot.disconnect()

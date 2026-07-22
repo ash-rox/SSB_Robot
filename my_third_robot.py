51f4ae8acf4f9d4b828237c4629c616ac05f869a
ashrox <ashtraddis@gmail.com>
2026-07-22 11:40:21 -0700

    Fix indentation for robot movement commands

--- my_third_robot.py
+++ my_third_robot.py
@@ -2,11 +2,11 @@ import time
 from sparkybotmini import SparkyBotMini
 robot = SparkyBotMini(port="/dev/ttyUSB0")
 if robot.connect():
-print("Moving forward!")
-robot.set_motor(40, 40, 40, 40) # All wheels forward at 40% speed
-time.sleep(2) # Keep moving for 2 seconds
-print("Stopping...")
-robot.set_motor(0, 0, 0, 0) # Stop all wheels
-time.sleep(1)
+  print("Moving forward!")
+  robot.set_motor(40, 40, 40, 40) # All wheels forward at 40% speed
+  time.sleep(2) # Keep moving for 2 seconds
+  print("Stopping...")
+  robot.set_motor(0, 0, 0, 0) # Stop all wheels
+  time.sleep(1)
 
 robot.disconnect()
